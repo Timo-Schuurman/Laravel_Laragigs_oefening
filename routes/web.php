247e7a993e9d6453cd142133b8cf5b9f5043963a
Timo <timoschuurman@outlook.com>
2023-09-13 16:29:15 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ListingController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +15,24 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//All Listings
+Route::get('/', [ListingController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Show Create form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store']);
+
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// Single Listing
+Route::get('/listing/{listing}', [ListingController::class, 'show']);
+
+
+
